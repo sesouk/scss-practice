123c67ed7e2e37982a7e5d51678fff4eaf34974f
Kevin Tran <sesouk@gmail.com>
2020-04-11 11:48:53 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<header class="showcase">
+			<div class="container">
+				<nav>
+					<h1 class="logo">My Website</h1>
+					<ul>
+						<li><a href="#1">Home</a></li>
+						<li><a href="#1">About</a></li>
+						<li><a href="#1">Services</a></li>
+					</ul>
+				</nav>
+
+				<div class="showcase-content">
+					<div>
+						<h1>Make Your Marketing Real</h1>
+						<p class="my-1">
+							Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est
+							eligendi tempore atque laborum. Quisquam nemo at non. Corrupti,
+							vitae dolore.
+						</p>
+						<a href="#1" class="btn-primary">Learn More</a>
+						<a href="#1" class="btn-secondary">Sign Up</a>
+					</div>
+					<img
+						src="https://themesbrand.com/zooki/layouts/images/home-2-img.png" alt='landing'
+					/>
+				</div>
+			</div>
+		</header>
   );
 }
 
